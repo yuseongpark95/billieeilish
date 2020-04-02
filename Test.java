a854df4a33339f0e08dd3b69c03b4c62293c1ac7
YuseongPark <pus1125@gmail.com>
2020-04-02 16:50:44 +0900

    Test.java 수정

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
    public static void main(String[] args){
 	System.out.println("Hello");
-	System.out.println("Hello World");
+	System.out.println("Hello Worldclear");
+	System.out.println("hotfix add");
    }
 }
